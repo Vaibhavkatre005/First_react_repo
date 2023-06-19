08d06a9f94cc6da37ed5085f94395f3e2a571163
Vaibhavkatre005 <vaibhavkatre005@gmail.com>
2023-06-19 22:58:57 +0530

    second try

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{backgroundColor:"gray"}}>
+        <h1 style={{color:"blue"}}>Sky is Blue</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
